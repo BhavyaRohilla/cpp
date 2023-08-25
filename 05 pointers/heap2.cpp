1d2cb220c28a2e4be03e2afc467b76251c7c5bf7
Bhavya Rohilla <officialbhavyarohilla@gmail.com>
2023-08-25 13:38:09 +0530

    night pain

---
+++ 05 pointers/heap2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter number of Elements => ";
+    cin >> size;
+    int *p = new int[size];
+
+    cout << "Enter new size => ";
+    cin >> size;
+    p = new int[size];
+    return 0;
+}
